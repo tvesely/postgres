460f9da842f4b7436a50f7d083b1a8a198ba37fb
Alexandra Wang <lewang@pivotal.io>
2019-08-14 00:31:49 +0000

    Generate dataset as part of install task

--- concourse/scripts/install_postgres.sh
+++ concourse/scripts/install_postgres.sh
@@ -24,3 +24,16 @@ popd
 pushd ${VOPS_SRC_PATH}
     USE_PGXS=true make -j32 install
 popd
+
+if [ -d ${HOME}/workspace/tpch-dbgen ]; then
+    export TPCH_DATAGEN_SRC_PATH=${HOME}/workspace/tpch-dbgen
+else
+    export TPCH_DATAGEN_SRC_PATH=${HOME}/tpch_dbgen_src
+fi
+
+# Generate dataset
+pushd ${TPCH_DATAGEN_SRC_PATH}
+    make -j32
+    ./dbgen -s ${SCALE_FACTOR} -T L -f
+    cp lineitem.tbl /tmp/lineitem.tbl
+popd
--- concourse/scripts/zedstore_perf_test.sh
+++ concourse/scripts/zedstore_perf_test.sh
@@ -14,18 +14,5 @@ else
     export POSTGRES_SRC_PATH=postgres_src
 fi
 
-if [ -d ${HOME}/workspace/tpch-dbgen ]; then
-    export TPCH_DATAGEN_SRC_PATH=${HOME}/workspace/tpch-dbgen
-else
-    export TPCH_DATAGEN_SRC_PATH=${HOME}/tpch_dbgen_src
-fi
-
-# Generate dataset
-pushd ${TPCH_DATAGEN_SRC_PATH}
-    make -j32
-    ./dbgen -s ${SCALE_FACTOR} -T L -f
-    cp lineitem.tbl /tmp/lineitem.tbl
-popd
-
 /usr/local/pgsql/bin/psql -a -f ${POSTGRES_SRC_PATH}/concourse/scripts/benchmark.sql | 
     perl -MTerm::ANSIColor -ane "if (/^(Time:|-- )/) { print color('bold blue'); print $_; print color('reset');} else { print $_ }"
