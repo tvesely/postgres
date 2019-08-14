be315b759a45c20ba7712b75bbb92be509541a91
Alexandra Wang <lewang@pivotal.io>
2019-08-14 00:37:57 +0000

    WIP: fix tpch-dbgen path

--- concourse/scripts/install_postgres.sh
+++ concourse/scripts/install_postgres.sh
@@ -28,7 +28,7 @@ popd
 if [ -d ${HOME}/workspace/tpch-dbgen ]; then
     export TPCH_DATAGEN_SRC_PATH=${HOME}/workspace/tpch-dbgen
 else
-    export TPCH_DATAGEN_SRC_PATH=${HOME}/tpch_dbgen_src
+    export TPCH_DATAGEN_SRC_PATH=tpch_dbgen_src
 fi
 
 # Generate dataset
